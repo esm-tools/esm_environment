fbe0d41a484fec52ef32377d1ff8436ca0b8a311
denizural <denizural86@gmail.com>
2020-09-07 13:41:13 +0200

    Bump version: 4.1.1 → 4.2.0

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dbarbi/esm_environment",
-    version="4.1.1",
+    version="4.2.0",
     zip_safe=False,
 )
