18f3071a63700b7c410cabb9b761642456df0933
denizural <denizural86@gmail.com>
2021-08-30 10:07:05 +0200

    Bump version: 5.1.1 → 5.1.2

--- esm_environment/__init__.py
+++ esm_environment/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Dirk Barbi"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "5.1.1"
+__version__ = "5.1.2"
 
 from .esm_environment import *
--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dbarbi/esm_environment",
-    version="5.1.1",
+    version="5.1.2",
     zip_safe=False,
 )
